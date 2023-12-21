67f3e83589b0556f520e64f20f7e3e1cd42da170
Bryce Sexton <brycesexton@Bryces-Laptop.local>
2023-12-21 14:48:29 -0800

    /greeting/bryce added

--- hw_part1/server.js
+++ hw_part1/server.js
@@ -1,15 +1,16 @@
-const express = require ('express')
-const app = express ()
+const express = require("express");
+const app = express();
+const port = 3000;
 
+app.get("/", (req, res) => {
+    res.send("Hello there!");
+  });
 
+app.get('/greeting/:name', (req, res) => {
+    const username = req.params.name
+    res.send(`Hello, ${username}!`);
+});
 
-
-
-
-
-
-
-
-app.listen(1999, () => {
-    console.log("im ready, im ready.")
-})
+app.listen(port, () => {
+  console.log(`Server is live on ${port}`);
+});
