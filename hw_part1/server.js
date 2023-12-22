afed18c550768c599da781b60b5a3a505c796c6a
Bryce Sexton <brycesexton@Bryces-Laptop.local>
2023-12-21 19:50:41 -0800

    final hw part 1

--- hw_part1/server.js
+++ hw_part1/server.js
@@ -5,17 +5,17 @@ const port = 3000;
 //greeting
 app.get("/", (req, res) => {
     res.send("Hello there!");
-  });
+})
 
 app.get('/greeting/:name', (req, res) => {
     const username = req.params.name
     res.send(`Hello, ${username}!`);
-});
+})
 
 //tip calculator
 app.get('/tip/:total/:tipPercentage', (req, res) => {
     res.send(`<h1>${req.params.total * (req.params.tipPercentage * .01)}</h1>`)
-});
+})
 
 //magic 8 ball
 app.get('/magic/:question', (req, res) => {
@@ -29,7 +29,7 @@ app.get('/magic/:question', (req, res) => {
 
   const magicResponse = responses[Math.floor(Math.random() * responses.length)];
   res.send(`<h1>Question: ${Q}</h1><h1>Answer: ${magicResponse}</h1>`);
-});
+})
 
 
 
@@ -43,4 +43,4 @@ app.get('/magic/:question', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Server is live on ${port}`);
-});
+})
--- passit_around/server.js
+++ passit_around/server.js
@@ -1,11 +1,30 @@
 const express = require ('express')
 const app = express ()
+app.use(express.urlencoded({extended: true}))
 
+let BEERS = 99;
 
+app.get('/', (req, res) => {
+    res.render('index', { BEERS });
+});
 
+app.get('/:number_of_bottles', (req, res) => {
+    const bottlesLeft = parseInt(req.params.number_of_bottles);
 
+    if (bottlesLeft === 0) {
+        res.render('noMoreBottles');
+    } else {
+        res.render('bottles', {
+            BEERS: bottlesLeft,
+            nextNumber: bottlesLeft - 1,
+        });
+    }
+});
 
-
+app.get('/startOver', (req, res) => {
+    BEERS = 99;
+    res.redirect('/');
+});
 
 
 
