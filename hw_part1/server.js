5784a6fdcc21bf76672e1c265ae896c51f959a44
Bryce Sexton <brycesexton@Bryces-Laptop.local>
2023-12-21 18:06:37 -0800

    magic 8 ball created

--- hw_part1/server.js
+++ hw_part1/server.js
@@ -17,6 +17,20 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
     res.send(`<h1>${req.params.total * (req.params.tipPercentage * .01)}</h1>`)
 });
 
+//magic 8 ball
+app.get('/magic/:question', (req, res) => {
+  const Q = req.params.Q.replace(/%20/g, ' ')
+  const responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely",
+      "You may rely on it", "As I see it yes", "Most likely", "Outlook good",
+      "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later",
+      "Better not tell you now", "Cannot predict now", "Concentrate and ask again",
+      "Don't count on it", "My reply is no", "My sources say no",
+      "Outlook not so good", "Very doubtful"]
+
+  const magicResponse = responses[Math.floor(Math.random() * responses.length)];
+  res.send(`<h1>Question: ${Q}</h1><h1>Answer: ${magicResponse}</h1>`);
+});
+
 
 
 
